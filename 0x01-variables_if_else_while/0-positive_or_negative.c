21053f4e286c7096ad56e19742ed9253c164705e
AhmedAtef283 <aatef83156@gmail.com>
2023-02-25 10:21:14 -0800

    a5r mra

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	int n;
@@ -11,16 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
+		printf("%i is positive\n", n);
 	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}	
+		printf("%i is negative\n", n);
 	else
-	{
-		printf("%d is zero\n:", n);
-	}
+		printf("%i is zero\n:", n);
 	return (0);
 }
